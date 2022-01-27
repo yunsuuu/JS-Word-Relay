36ddde5ea208683d029f80242ed50edbd138f31c
dbstnqls6953@naver.com <dbstnqls6953@naver.com>
2022-01-27 18:51:54 +0900

    기능 업데이트

--- app.js
+++ app.js
@@ -3,40 +3,47 @@ const $word = document.querySelector(".word");
 const $form = document.querySelector("form");
 const $input = document.querySelector("input");
 const $btn = document.querySelector("button");
-const number = prompt("몇 명이 참여하나요?", "2명");
+const number = parseInt(prompt("몇 명이 참여하나요?", 2));
 
-if(number === null){
-  alert("참가자 수를 적어주세요!");
-  prompt("몇 명이 참여하나요?", "2명");
-  // prompt창에 인원 수 입력할 때까지 alert 반복해서 띄어줌
-}
-
-let word;
-let newWord;
-
-const handleSubmit = (e) => {
-  e.preventDefault();
+// prompt를 취소하면 null이 출력
+// null은 number 함수에 들어가면 NaN
+// NaN은 if문에 들어가면 항상 false
+// number가 null(취소를 누른 경우)이면 if문 내부는 실행되지 않음
+if(number){ 
+  // prompt창에서 확인을 누르면 아래 코드 실행
+  let word;
+  let newWord;
   
-  if(!word || word.substr(-1, 1) === newWord && newWord.lenght === 3 ){
-    word = newWord;
-    $word.innerText = word;
+  const handleSubmit = (e) => {
+    e.preventDefault();
     
-    const order = parseInt($order.innerText);
-    if(order >= parseInt(number)){
+    if(newWord.length !== 3){
+      alert("세글자 단어를 입력해 주세요!");
+    } 
+    else if (!word || word.substr(-1, 1) === newWord[0]){
+      word = newWord;
+      $word.innerText = word;
+      const order = parseInt($order.innerText);
+      if(order + 1 > number){
         $order.innerText = 1;
-    } else {
+      } else {
         $order.innerText = order + 1;
+      }
+    } 
+    else {
+      alert("제시어를 살펴본 후 올바른 세글자 단어를 입력해 주세요!");
     }
-  } else {
-    alert("세글자를 가진 올바른 단어를 입력하세요!");
+    $input.value = "";
+    $input.focus();
   }
-  $input.value = "";
-  $input.focus();
+  
+  const handleInput = (e) => {
+    newWord = e.target.value;
+  }
+  
+  $form.addEventListener("submit", handleSubmit);
+  $input.addEventListener("input", handleInput);
+} 
+else {
+  alert("참여인원을 숫자로 입력해 주세요!");
 }
-
-const handleInput = (e) => {
-  newWord = e.target.value;
-}
-
-$form.addEventListener("submit", handleSubmit);
-$input.addEventListener("input", handleInput);
